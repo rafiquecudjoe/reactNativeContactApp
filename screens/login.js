5664ea4bfcfe96af57970b00ed9a33fcbdb43af9
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2021-10-02 07:30:21 +0000

    added memberProfile and myProfile screens

--- screens/login.js
+++ screens/login.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity  } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 import pic1 from "../assets/home2.jpg";
 
@@ -7,31 +14,32 @@ function Welcome({ navigation }) {
   return (
     <View style={styles.main}>
       <Image style={styles.welcomeimg} source={pic1} />
-      <View style={{ marginTop: 80,alignSelf:"flex-start",marginHorizontal:20 }}>
+      <View
+        style={{ marginTop: 80, alignSelf: "flex-start", marginHorizontal: 20 }}
+      >
         <View style={styles.input1}>
-          <Text style={{fontSize:20,marginBottom:20,}}> Email</Text>
-                  <TextInput style={styles.email }placeholder="john.smith@gmail.com" />
+          <Text style={{ fontSize: 20, marginBottom: 20 }}> Email</Text>
+          <TextInput style={styles.email} placeholder="john.smith@gmail.com" />
         </View>
         <View style={styles.input2}>
-          <Text style={{fontSize:20}}> Password</Text>
-          <TextInput style={styles.pass } placeholder="..." />
+          <Text style={{ fontSize: 20 }}> Password</Text>
+          <TextInput style={styles.pass} placeholder="..." />
         </View>
-          </View>
+      </View>
       <View style={styles.signin}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("myProfile")}>
           <Text style={styles.signin1}>SIGN IN</Text>
         </TouchableOpacity>
-          </View>
+      </View>
 
       <View style={styles.footer}>
         <View style={styles.resetp}>
-          <TouchableOpacity onPress={() => navigation.navigate("signUp")}>
+          <TouchableOpacity onPress={() => navigation.navigate("")}>
             <Text style={styles.res}>Forgot?</Text>
           </TouchableOpacity>
 
           <Text style={styles.border}>Reset Password</Text>
         </View>
-      
       </View>
     </View>
   );
@@ -40,7 +48,7 @@ function Welcome({ navigation }) {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    
+
     alignItems: "center",
   },
   welcomeimg: {
@@ -50,48 +58,42 @@ const styles = StyleSheet.create({
 
   input1: {
     flexDirection: "row",
-      alignItems: "center",
-    alignSelf: 'flex-start',
+    alignItems: "center",
+    alignSelf: "flex-start",
     borderBottomWidth: 0.2,
-    borderColor: 'grey',
-     marginBottom:10
-     
+    borderColor: "grey",
+    marginBottom: 10,
   },
   input2: {
     flexDirection: "row",
-      alignItems: "center",
-     alignSelf:'flex-start'
-     
+    alignItems: "center",
+    alignSelf: "flex-start",
   },
-    email:{paddingLeft:135,fontSize:17},
-    pass: { paddingLeft: 250, fontSize: 17 },
-  resetp: { flexDirection: 'row' },
+  email: { paddingLeft: 135, fontSize: 17 },
+  pass: { paddingLeft: 250, fontSize: 17 },
+  resetp: { flexDirection: "row" },
   signin: {
     marginVertical: 40,
-    backgroundColor: '#FF2442',
+    backgroundColor: "#FF2442",
     height: 45,
     width: 330,
     borderRadius: 5,
     justifyContent: "center",
-    
   },
   signin1: {
     alignSelf: "center",
-    color:'white'
-    
+    color: "white",
   },
   footer: {
     alignSelf: "flex-start",
-    marginLeft:30
-    
-  }, res: {
-   marginRight:5
+    marginLeft: 30,
+  },
+  res: {
+    marginRight: 5,
   },
   border: {
     borderBottomWidth: 1.5,
-    borderBottomColor:'red'
-  }
-
- 
+    borderBottomColor: "red",
+  },
 });
 export default Welcome;
